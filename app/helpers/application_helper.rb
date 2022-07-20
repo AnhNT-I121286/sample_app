e514e07722b300e3d15bac38a840e7cf8f17cb41
teumiloo <nguyentienanh20122000@gmail.com>
2022-07-20 09:56:19 +0700

    Chapter_10

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,11 +5,4 @@ module ApplicationHelper
     base_title = t ".base_title"
     page_title.blank? ? base_title : "#{page_title} | #{base_title}"
   end
-
-  # Returns the Gravatar for the given user.
-  def gravatar_for(user)
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar")
-  end
 end
